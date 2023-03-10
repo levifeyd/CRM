618ba063d5b95448905c53fd399c9c07074e89d2
levifeyd <kuleshov-as@mail.ru>
2023-03-10 12:52:03 +0400

    add view request form

---
+++ project/resources/views/layout/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title')</title>
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+</head>
+<body>
+@yield('content')
+<script src="resources/js/app.js"></script>
+</body>
+</html>
---
+++ project/resources/views/partials/header.blade.php
@@ -0,0 +1,3 @@
+<div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
+    <h1 class="font-bold text-2xl text-gray-800">"Custom CRM"</h1>
+</div>

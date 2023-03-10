4dd70e16d58af3fb8a35927b41f150d94a3c9212
levifeyd <kuleshov-as@mail.ru>
2023-03-10 12:13:24 +0400

    create migrations, models and seeders

---
+++ project/database/seeders/ManagerSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Manager;
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ManagerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $manager = Manager::query()->create([
+            'username'=>'Artem',
+            'password'=>bcrypt('qwerty'),
+            'login_date'=>Carbon::createFromFormat('Y-m-d', '2023-03-10')
+        ]);
+        $manager = Manager::query()->create([
+            'username'=>'Polina',
+            'password'=>bcrypt('qwerty'),
+            'login_date'=>Carbon::createFromFormat('Y-m-d', '2023-03-10')
+        ]);
+    }
+}
